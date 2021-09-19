fef9743a509a0cda047b3ed8652c713c61d4d553
DjNovovic <djordjijenovovic@gmail.com>
2021-09-19 12:49:00 +0200

    Initial Commit

---
+++ TryHaptics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TryHaptics
+//
+//  Created by Đorđije Novović on 19.9.21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TryHaptics/TryHapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryHapticsApp.swift
+//  TryHaptics
+//
+//  Created by Đorđije Novović on 19.9.21..
+//
+
+import SwiftUI
+
+@main
+struct TryHapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
